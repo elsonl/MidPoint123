774501f7b8949f8e80e2220f43f81d5cc77b60f9
Elson Lin <s033046@students.lmsd.org>
2022-02-17 19:16:25 -0500

    Initial Commit

---
+++ MidPoint/MidPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidPointApp.swift
+//  MidPoint
+//
+//  Created by Elson Lin (student LM) on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct MidPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
