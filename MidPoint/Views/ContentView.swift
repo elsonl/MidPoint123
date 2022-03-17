bfa680662807f83b16c3b3f7650c53827d226606
TOdonn <s015862@students.lmsd.org>
2022-03-17 12:40:46 -0400

    no more location manager

--- MidPoint/Views/ContentView.swift
+++ MidPoint/Views/ContentView.swift
@@ -17,32 +17,13 @@ struct ContentView: View {
     @State private var showsheet1 = false
     @State private var showsheet2 = false
     
-    
-    @StateObject var locationManager = LocationManager()
-       
-       var userLatitude: String {
-           return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
-       }
-       
-       var userLongitude: String {
-           return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
-       }
-    
+
     var body: some View {
         
         NavigationView{
             ZStack{
                 Color.BackgroundColor
                 VStack{
-                    
-                    VStack {
-                               Text("location status: \(locationManager.statusString)")
-                               HStack {
-                                   Text("latitude: \(userLatitude)")
-                                   Text("longitude: \(userLongitude)")
-                               }
-                           }
-                    
                     Text("Enter Locations Below")
                         .foregroundColor(Color.TextFieldText)
                     
