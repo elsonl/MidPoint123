f45cdef042435c8ef79d1aaf114990dccfc07f76
Andy Sun <sunandy07@gmail.com>
2022-04-06 14:41:14 -0400

    Fixed Favorites bug and added a arrow button

--- MidPoint/Views/ContentView.swift
+++ MidPoint/Views/ContentView.swift
@@ -20,6 +20,7 @@ struct ContentView: View {
     @State var favoritesName : String = ""
     @State var favoritesAddress : String = ""
     @State var favoritesOpen : Bool = false
+    @State var favoritesToggle : Bool = false
     
     var body: some View {
         
@@ -30,156 +31,176 @@ struct ContentView: View {
                 Color.white
                 //Image().aspectRatio image here resize
                 VStack{
-                    Image("InAppIcon").opacity(0.5)
-                    Text("MidPoint")
-                    //No Space Here, no time to fix
-                    Text("Enter Locations Below")//.font(<#T##font: Font?##Font?#>)
-                        .foregroundColor(Color.TextFieldText)
-                        .padding()
-                    
-                    //Address1
-                    HStack{
-                        Image(systemName: "magnifyingglass")
+                    Group{
+                        Image("InAppIcon").opacity(0.5)
+                        Text("MidPoint")
+                        //No Space Here, no time to fix
+                        Text("Enter Locations Below")//.font(<#T##font: Font?##Font?#>)
+                            .foregroundColor(Color.TextFieldText)
+                            .padding()
                         
-                        TextField("Location 1 ...", text: $address1,onEditingChanged: { _ in
-                            geocoding.count = true
-                            print("1, count true")
-                            if (address1 == "") {
-                                showsheet1 = true
-                                print("Address1 change, showsheet true")
-                            }
-                        }, onCommit: {
-                            showsheet1 = false
-                            print("address1 committed")
-                        })
-                            .foregroundColor(Color.black).background(Color(.systemGray4)).textFieldStyle(RoundedBorderTextFieldStyle())
-                            .sheet(isPresented : $showsheet1, onDismiss: {
-                                
-                                if(address1 == ""){
-                                    print("address 1 empty, showsheet true")
+                        //Address1
+                        HStack{
+                            Image(systemName: "magnifyingglass")
+                            
+                            TextField("Location 1 ...", text: $address1,onEditingChanged: { _ in
+                                geocoding.count = true
+                                print("1, count true")
+                                if (address1 == "") {
                                     showsheet1 = true
-                                } else {
-                                    geocoding.count = true
-                                    
-                                    print(geocoding.count)
-                                    print("in else")
-                                    
-                                    print("showsheet1false")
-                                    showsheet1 = false
-                                    geocoding.getData()
-                                    
+                                    print("Address1 change, showsheet true")
                                 }
-                            }) { PlacesAutoComplete(geocoding: _geocoding, dMatrix: _dMatrix, address1: $address1, address2: $address2, favoritesAddress: $favoritesAddress)
-                                .environmentObject(geocoding) }
-                    }.frame(width: 375, alignment: .trailing)
-                    
-                    Spacer().frame(height: 25)
-                    
-                    //Address 2
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                        TextField("Location 2 ...", text: $address2,onEditingChanged: { _ in
-                            geocoding.count = false
-                            print("2, count false")
-                            if (address2 == "") {
-                                showsheet2 = true
-                                print("Address2 change, showsheet true")
-                            }
-                        }, onCommit: {
-                            showsheet2 = false
-                            print("address2 committed")
-                        }).foregroundColor(Color.black).background(Color(.systemGray4)).textFieldStyle(RoundedBorderTextFieldStyle())
-                        .sheet(isPresented : $showsheet2, onDismiss: {
-                            if(address2 == ""){
-                                print("address 2 empty, showsheet true")
-                                showsheet2 = true
-                            } else {
-                                geocoding.count = false
-                                
-                                print(geocoding.count)
-                                print("in else")
-                                
-                                print("showsheet2false")
-                                showsheet2 = false
-                                geocoding.getData()
-                                
-                                
-                            }
-                        }) { PlacesAutoComplete(geocoding: _geocoding, address1: $address1, address2: $address2, favoritesAddress: $favoritesAddress)
-                            .environmentObject(geocoding) }}.frame(width: 375, alignment: .trailing)
-                    
-                    Spacer().frame(height: 15)
-                    
-                    //Search Button
-                    
-                    Button(action : {
+                            }, onCommit: {
+                                showsheet1 = false
+                                print("address1 committed")
+                            })
+                                .foregroundColor(Color.black).background(Color(.systemGray4)).textFieldStyle(RoundedBorderTextFieldStyle())
+                                .sheet(isPresented : $showsheet1, onDismiss: {
+                                    
+                                    if(address1 == ""){
+                                        print("address 1 empty, showsheet true")
+                                        showsheet1 = true
+                                    } else {
+                                        geocoding.count = true
+                                        
+                                        print(geocoding.count)
+                                        print("in else")
+                                        
+                                        print("showsheet1false")
+                                        showsheet1 = false
+                                        geocoding.getData()
+                                        
+                                    }
+                                }) { PlacesAutoComplete(geocoding: _geocoding, dMatrix: _dMatrix, address1: $address1, address2: $address2, favoritesAddress: $favoritesAddress)
+                                    .environmentObject(geocoding) }
+                        }.frame(width: 375, alignment: .trailing)
                         
-                    }, label: {
-                        NavigationLink(destination : GMapsView()){
+                        Spacer().frame(height: 25)
+                        
+                        //Address 2
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                            TextField("Location 2 ...", text: $address2,onEditingChanged: { _ in
+                                geocoding.count = false
+                                print("2, count false")
+                                if (address2 == "") {
+                                    showsheet2 = true
+                                    print("Address2 change, showsheet true")
+                                }
+                            }, onCommit: {
+                                showsheet2 = false
+                                print("address2 committed")
+                            }).foregroundColor(Color.black).background(Color(.systemGray4)).textFieldStyle(RoundedBorderTextFieldStyle())
+                                .sheet(isPresented : $showsheet2, onDismiss: {
+                                    if(address2 == ""){
+                                        print("address 2 empty, showsheet true")
+                                        showsheet2 = true
+                                    } else {
+                                        geocoding.count = false
+                                        
+                                        print(geocoding.count)
+                                        print("in else")
+                                        
+                                        print("showsheet2false")
+                                        showsheet2 = false
+                                        geocoding.getData()
+                                        
+                                        
+                                    }
+                                }) { PlacesAutoComplete(geocoding: _geocoding, address1: $address1, address2: $address2, favoritesAddress: $favoritesAddress)
+                                    .environmentObject(geocoding) }}.frame(width: 375, alignment: .trailing)
+                        
+                        Spacer().frame(height: 15)
+                        
+                        //Search Button
+                        
+                        Button(action : {
                             
-                            Text("Search")
-                            
-                        }
-                    }).padding().background(Color.FavoritesBackground).clipShape(Capsule()).foregroundColor(.black)
-                    
-                    Spacer().frame(height: 35)
-                    
+                        }, label: {
+                            NavigationLink(destination : GMapsView()){
+                                
+                                Text("Search")
+                                
+                            }
+                        }).padding().background(Color.FavoritesBackground).clipShape(Capsule()).foregroundColor(.black)
+                        
+                        Spacer().frame(height: 35)
+                    }
                     //
                     //Favorites
-                    Text("Favorites")
-//                    ZStack{
-//                        //Grey Background
-//                        Rectangle().foregroundColor(Color.yellow).frame(width: 350, height: 140, alignment: .center).cornerRadius(35).padding()
-//                        VStack{
-//                            
-//                            ScrollView(.horizontal){
-//                                
-//                                HStack(spacing : 15){
-//                                    //Add/Edit Favorites
-//                                    Button(action: {print("button pressed")}, label: {
-//                                        
-//                                        NavigationLink(destination: FavoritesView(favoritesName: $favoritesName, favoritesAddress: $favoritesAddress, favorites: $favorites, address1: $address1, address2: $address2, favoritesOpen: $favoritesOpen), isActive: $favoritesOpen, label: {
-//                                            
-//                                            Button(action: {favoritesOpen=true}, label: {
-//                                                VStack{
-//                                                    //Text Inside
-//                                                    Text("Add/Edit").padding([.top, .horizontal])
-//                                                    Text("Favorites").padding([.bottom, .horizontal])
-//                                                }
-//                                                
-//                                            }).frame(width: 120, height: 100, alignment: .center)
-//                                            
-//                                        }).onDisappear(perform:{
-//                                            
-//                                            favorites.updateValue(favoritesAddress, forKey: favoritesName)
-//                                            
-//                                            
-//                                            
-//                                            
-//                                        })
-//                                        
-//                                        NavigationLink(destination: EmptyView()){
-//                                            EmptyView()
-//                                        }
-//                                    })
-//                                        .background(Color.black)
-//                                        .clipShape(RoundedRectangle(cornerRadius: 20))
-//                                        .foregroundColor(.white)
-//                                        .frame(width: 100, height: 100, alignment: .center)
-//                                        .padding(.trailing, 9)
-//                                        .padding(.leading, 15)
-//                                    //Red Favorites
-//                                    ForEach(favorites.keys.sorted(), id: \.self){
-//                                        Text($0)
-//                                    }.frame(width: 100, height: 100, alignment: .center)
-//                                        .background(RoundedRectangle(cornerRadius: 20).foregroundColor(.gray))
-//                                       
-//                                    
-//                                }
-//                            }.padding().frame(width: 381, height: 125, alignment: .center)
-//                        }
-//                    }
-                    //
+                    
+                    HStack{
+                        Spacer()
+                        Spacer()
+                        Text("Favorites")
+                        Spacer()
+                        Spacer()
+                        Button(action: {favoritesToggle.toggle()}){
+                            if(favoritesToggle){
+                                Image(systemName: "arrowtriangle.down.fill")
+                            }else{
+                                Image(systemName: "arrowtriangle.down.fill").rotationEffect(Angle(radians: -Double.pi/2.0))
+                            }
+                        }
+                        Spacer()
+                        
+                    }
+                    
+                    Group{
+                        ZStack {
+                            //Grey Background
+                            Rectangle().foregroundColor(Color.yellow).frame(width: 350, height: 140, alignment: .center).cornerRadius(35).padding()
+                            VStack{
+                                ScrollView(.horizontal){
+                                    
+                                    HStack(spacing : 15){
+                                        //Add/Edit Favorites
+                                        
+                                        Button(action: {print("button pressed")}, label: {
+                                            
+                                            NavigationLink(destination: FavoritesView(favoritesName: $favoritesName, favoritesAddress: $favoritesAddress, favorites: $favorites, address1: $address1, address2: $address2, favoritesOpen: $favoritesOpen), isActive: $favoritesOpen, label: {
+                                                
+                                                Button(action: {favoritesOpen=true}, label: {
+                                                    VStack{
+                                                        //Text Inside
+                                                        Group{
+                                                            Text("Add/Edit").padding([.top, .horizontal])
+                                                            Text("Favorites").padding([.bottom, .horizontal])
+                                                        }
+                                                    }
+                                                    
+                                                }).frame(width: 120, height: 100, alignment: .center)
+                                                
+                                            }).onDisappear(perform:{
+                                                
+                                                favorites.updateValue(favoritesAddress, forKey: favoritesName)
+                                                
+                                            })
+                                            
+                                            NavigationLink(destination: EmptyView()){
+                                                EmptyView()
+                                            }
+                                        })
+                                            .background(Color.black)
+                                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                                            .foregroundColor(.white)
+                                            .frame(width: 100, height: 100, alignment: .center)
+                                            .padding(.trailing, 9)
+                                            .padding(.leading, 15)
+                                        
+                                        //Red Favorites
+                                        
+                                        ForEach(favorites.keys.sorted(), id: \.self){
+                                            Text($0)
+                                        }.frame(width: 100, height: 100, alignment: .center)
+                                            .background(RoundedRectangle(cornerRadius: 20).foregroundColor(.gray))
+                                        
+                                        
+                                    }
+                                }.padding().frame(width: 381, height: 125, alignment: .center)
+                            } //VStack Close
+                        } //ZStack Close
+                    }
                 }
             }.ignoresSafeArea()
         }
